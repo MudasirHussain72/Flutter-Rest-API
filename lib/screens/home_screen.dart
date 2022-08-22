ead92a373481ce62a0bea891a342f9563879e816
MudasirHussain72 <mudasirhussain41302@gmail.com>
2022-08-22 21:28:35 +0500

    Get API call

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,88 @@
+import 'dart:convert';
+import 'package:api/models/posts_model.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<PostsModel> postList = [];
+  Future<List<PostsModel>> getPostApi() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      for (Map i in data) {
+        postList.add(PostsModel.fromJson(i));
+      }
+      return postList;
+    } else {
+      return postList;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Api course"),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: FutureBuilder(
+                future: getPostApi(),
+                builder: (context, snapShot) {
+                  if (!snapShot.hasData) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  } else {
+                    return ListView.builder(
+                      itemCount: postList.length,
+                      itemBuilder: ((context, index) {
+                        return Card(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                postList[index].userId.toString(),
+                              ),
+                              Text(
+                                postList[index].id.toString(),
+                              ),
+                              Text(
+                                'Title',
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                postList[index].title.toString(),
+                              ),
+                              Text(
+                                'Discription',
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                postList[index].body.toString(),
+                              ),
+                            ],
+                          ),
+                        );
+                      }),
+                    );
+                  }
+                }),
+          ),
+        ],
+      ),
+    );
+  }
+}
